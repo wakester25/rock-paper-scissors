9cfcee98e634f1257c576d9d58e8de192cae1e3d
wakester <wakester2@gmail.com>
2022-11-11 16:59:43 -0500

    Create function to get user choice

--- code.js
+++ code.js
@@ -1,5 +1,16 @@
 
 function getComputerChoice() {
-    const choices = ['Rock', 'Paper', 'Scissors'];
+    const choices = ['Rock', 'Paper', 'Scissor'];
     return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function getPlayerChoice() {
+    let playerInput = prompt("Please enter your choice (Rock, Paper, Scissor): ").toLowerCase();
+    playerInput = playerInput.charAt(0).toUpperCase() + playerInput.slice(1);
+    
+    if (playerInput === 'Rock' || playerInput === 'Paper' || playerInput === 'Scissor') {
+        return playerInput;
+    } else {
+        return "Error: Invalid choice selected";
+    }
 }
